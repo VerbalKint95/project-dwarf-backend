17248da9abdb52ce4753b7c89066d0179f2846ee
VerbalKint95 <constantin@cy-tech.fr>
2024-06-05 12:26:16 -0500

    add cors

---
+++ project-dwarf/src/main/java/com/maligno/projectdwarf/springboot/security/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package com.maligno.projectdwarf.springboot.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("*") // Autoriser les requêtes de n'importe quelle origine
+            .allowedMethods("GET", "POST", "PUT", "DELETE") // Autoriser certaines méthodes HTTP
+            .allowedHeaders("*"); // Autoriser tous les en-têtes
+    }
+}
