89d7c548b83fc241291d3c84a15bed226e8f4708
VerbalKint95 <constantin@cy-tech.fr>
2024-06-06 21:03:07 -0500

    fix tables

--- project-dwarf/src/main/java/com/maligno/projectdwarf/springboot/model/Comment.java
+++ project-dwarf/src/main/java/com/maligno/projectdwarf/springboot/model/Comment.java
@@ -9,8 +9,6 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -37,7 +35,7 @@ public class Comment {
 	@Column(unique = true)
 	private String content;
 	
-	@OneToMany
+	@ManyToOne
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
 	
--- project-dwarf/src/main/java/com/maligno/projectdwarf/springboot/model/Topic.java
+++ project-dwarf/src/main/java/com/maligno/projectdwarf/springboot/model/Topic.java
@@ -7,8 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -34,7 +33,7 @@ public class Topic {
 	@Column(unique = true)
 	private String content;
 	
-	@OneToMany
+	@ManyToOne
     @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
 	
